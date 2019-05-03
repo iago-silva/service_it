1889e187c68b7370d7bb043df3ae085686326f2f
Iago Silva <iago.silva@interage.in>
2019-05-03 00:04:13 -0300

    Add simplecov to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,10 @@
+require 'simplecov'
+
+SimpleCov.minimum_coverage 100
+SimpleCov.start do
+  add_filter '/spec/'
+end
+
 $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
 
 require 'service_it'
